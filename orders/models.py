fd4b23720d51a6f8e82f89adbff8023c269d9c86
akfaqih3 <88878961+akfaqih3@users.noreply.github.com>
2025-08-05 17:56:56 +0300

    19736

--- orders/models.py
+++ orders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class MenuItem(models.Model):
+class Menu(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
@@ -31,7 +31,7 @@ class Order(models.Model):
         help_text="Teh customer who placed the order."
     )
     order_items = models.ManyToManyField(
-        MenuItem,
+        Menu,
         related_name='orders',
         help_text="The menu items included in this order."
     )
