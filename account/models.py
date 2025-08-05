3777a52f3973bb3d78407eea081ccfeb78f8342f
akfaqih3 <88878961+akfaqih3@users.noreply.github.com>
2025-08-05 23:04:51 +0300

    19737

--- account/models.py
+++ account/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
 
-    user = models.OneToOneField(User, on_delete=models.ON_CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=100,blank=True)
     phone = models.CharField(max_length=20)
 
