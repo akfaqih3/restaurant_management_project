8279f924297008e5982b9ac86ecc087a635e6186
akfaqih3 <88878961+akfaqih3@users.noreply.github.com>
2025-08-05 23:20:54 +0300

    19737

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,5 +1,5 @@
-from rest_framework import serializers
-from .models import Menu
+from rest_framework import serializers 
+from .models import Menu 
 
 
 class MenuSerializer(serializers.ModelSerializer):
