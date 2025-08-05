76c70e52b537a8905d4e969f041e8e224e01c24c
akfaqih3 <88878961+akfaqih3@users.noreply.github.com>
2025-08-05 17:39:35 +0300

    19735

--- orders/models.py
+++ orders/models.py
@@ -1,14 +1,1 @@
-from django.db import models
-from django.contrib.auth.models import User
-# Create your models here.
-
-class Profile(models.Model):
-
-    user = models.OneToOneField(User,on_delete=models.ON_CASCADE)
-
-    name = models.CharField(max_length=100,blank=True)
-    
-    phone = models.CharField(max_length=20)
-
-    def __str__(self):
-        return self.user.username
+from django.db import 
