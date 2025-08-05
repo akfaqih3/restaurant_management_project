fabbb7f720526d11285282d38f220cea767e5802
akfaqih3 <88878961+akfaqih3@users.noreply.github.com>
2025-08-05 23:41:17 +0300

    19734

--- home/views.py
+++ home/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from orders.models import Menu
 # Create your views here.
+
+def home(request):
+    menu = Menu.objects.all()
+    return render(request, 'home.index', context={'menu':menu})
