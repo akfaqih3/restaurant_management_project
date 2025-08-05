d8a5fad405a1aeb174cfa01db9261b390884ac70
akfaqih3 <88878961+akfaqih3@users.noreply.github.com>
2025-08-05 23:23:34 +0300

    19737

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .serializers import MenuSerializer 
+from .models import Menu
+from rest_framework.generics import ListAPIView
 
-# Create your views here.
+
+
+class MenuAPIView(ListAPIView):
+
+    queryset = Menu.objects.all()
+    fields = "__all__"
