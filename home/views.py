df0970fec822dc78b560842b27718e23e3ea5d80
akfaqih3 <88878961+akfaqih3@users.noreply.github.com>
2025-08-11 22:58:24 +0300

    23100

--- home/views.py
+++ home/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 from orders.models import Menu
-# Create your views here.
+from djanog.conf import settings
+
 
 def home(request):
     menu = Menu.objects.all()
+    context = {
+        "menu":menu,
+        "APP_NAME":settings.APP_NAME
+    }
     return render(request, 'home.html', context={'menu':menu})
 
 
+
 def custom_404(request, exception):
+    """
+    This function to handle page not found error
+    """
     return render(request,'404.html', status=404)
